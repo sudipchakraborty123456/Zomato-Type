28fd4e61ee802c2f5a4554c27d2d727c76b06b35
sudipchakraborty123456 <52750175+sudipchakraborty123456@users.noreply.github.com>
2021-05-29 22:13:33 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
 const DEVELOPMENT_URL = "http://localhost:5420";
-const PRODUCTION_URL = "https://zomato-type-website-backend.herokuapp.com/"; //got from heroku
+const PRODUCTION_URL = "https://zomato-type-website-backend.herokuapp.com"; //got from heroku
 exports.API_URL = PRODUCTION_URL;
